be496b7aba1bae983fe4d84a00823032e466dee4
Eelco Los <5102501+EelcoLos@users.noreply.github.com>
2024-07-15 11:18:19 +0200

    changed to fastendpoints

--- apps/api-demo/Program.cs
+++ apps/api-demo/Program.cs
@@ -1,6 +1,51 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using FastEndpoints;
+using FastEndpoints.Swagger; //add this
 
-app.MapGet("/", () => "Hello World!");
+var bld = WebApplication.CreateBuilder();
+bld.Services
+   .AddFastEndpoints()
+   .SwaggerDocument(o =>
+{
+	o.DocumentSettings = s =>
+	{
+		s.Title = "My API";
+		s.Version = "v1";
+	};
+});
 
+var app = bld.Build();
+app.UseFastEndpoints()
+   .UseSwaggerGen(); //add this
 app.Run();
+
+
+public class MyRequest
+{
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+	public int Age { get; set; }
+}
+
+public class MyResponse
+{
+	public string FullName { get; set; }
+	public bool IsOver18 { get; set; }
+}
+
+public class MyEndpoint : Endpoint<MyRequest, MyResponse>
+{
+	public override void Configure()
+	{
+		Post("/api/user/create");
+		AllowAnonymous();
+	}
+
+	public override async Task HandleAsync(MyRequest req, CancellationToken ct)
+	{
+		await SendAsync(new()
+		{
+			FullName = req.FirstName + " " + req.LastName,
+			IsOver18 = req.Age > 18
+		});
+	}
+}
