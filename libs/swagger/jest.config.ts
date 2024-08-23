add92053941900a14ee6ac191d85423ebb7bb7e4
Eelco Los <5102501+EelcoLos@users.noreply.github.com>
2024-08-23 10:21:45 +0200

    skipped test for config

--- libs/swagger/jest.config.ts
+++ libs/swagger/jest.config.ts
@@ -8,5 +8,5 @@ export default {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/libs/swagger',
-  passwithNoTests: true,
+  passWithNoTests: true,
 };
---
+++ libs/swagger/src/lib/swagger.spec.ts
@@ -0,0 +1,5 @@
+describe('Sample Test Suite', () => {
+  test.skip('This is a skipped test', () => {
+    expect(true).toBe(true);
+  });
+});
