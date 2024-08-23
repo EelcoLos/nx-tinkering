e18a39ea4aca71b1ab3122a451daf903bbf7b229
Eelco Los <5102501+EelcoLos@users.noreply.github.com>
2024-08-23 09:15:48 +0200

    lint: fixes

--- apps/api-demo.Test/UnitTest1.cs
+++ apps/api-demo.Test/UnitTest1.cs
@@ -2,9 +2,9 @@ namespace ApiDemo.Test;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
-    {
+  [Fact]
+  public void Test1()
+  {
 
-    }
+  }
 }
--- apps/api-demo/Program.cs
+++ apps/api-demo/Program.cs
@@ -6,11 +6,11 @@ bld.Services
    .AddFastEndpoints()
    .SwaggerDocument(o =>
 {
-	o.DocumentSettings = s =>
-	{
-		s.Title = "My API";
-		s.Version = "v1";
-	};
+  o.DocumentSettings = s =>
+  {
+    s.Title = "My API";
+    s.Version = "v1";
+  };
 });
 
 var app = bld.Build();
@@ -21,31 +21,31 @@ app.Run();
 
 public class MyRequest
 {
-	public string FirstName { get; set; }
-	public string LastName { get; set; }
-	public int Age { get; set; }
+  public string FirstName { get; set; }
+  public string LastName { get; set; }
+  public int Age { get; set; }
 }
 
 public class MyResponse
 {
-	public string FullName { get; set; }
-	public bool IsOver18 { get; set; }
+  public string FullName { get; set; }
+  public bool IsOver18 { get; set; }
 }
 
 public class MyEndpoint : Endpoint<MyRequest, MyResponse>
 {
-	public override void Configure()
-	{
-		Post("/api/user/create");
-		AllowAnonymous();
-	}
+  public override void Configure()
+  {
+    Post("/api/user/create");
+    AllowAnonymous();
+  }
 
-	public override async Task HandleAsync(MyRequest req, CancellationToken ct)
-	{
-		await SendAsync(new()
-		{
-			FullName = req.FirstName + " " + req.LastName,
-			IsOver18 = req.Age > 18
-		});
-	}
+  public override async Task HandleAsync(MyRequest req, CancellationToken ct)
+  {
+    await SendAsync(new()
+    {
+      FullName = req.FirstName + " " + req.LastName,
+      IsOver18 = req.Age > 18
+    });
+  }
 }
