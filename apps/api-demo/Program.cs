896372f53aef971c81285053e3c37bb90ce89370
Eelco Los <5102501+EelcoLos@users.noreply.github.com>
2024-08-23 10:18:41 +0200

    add swagger project

--- apps/api-demo/Program.cs
+++ apps/api-demo/Program.cs
@@ -1,7 +1,13 @@
 using FastEndpoints;
 using FastEndpoints.Swagger; //add this
+using FastEndpoints.ClientGen.Kiota;
+using Kiota.Builder;
 
-var bld = WebApplication.CreateBuilder();
+var bld = WebApplication.CreateBuilder(args);
+bld.Services.AddOutputCache(options =>
+{
+  options.AddBasePolicy(builder => builder.Cache());
+});
 bld.Services
    .AddFastEndpoints()
    .SwaggerDocument(o =>
@@ -13,9 +19,28 @@ bld.Services
   };
 });
 
+
 var app = bld.Build();
 app.UseFastEndpoints()
    .UseSwaggerGen(); //add this
+
+var outputPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "libs", "swagger", "src", "lib");
+
+// Ensure the directory exists
+if (!Directory.Exists(outputPath))
+{
+  Directory.CreateDirectory(outputPath);
+}
+await app.GenerateApiClientsAndExitAsync(
+    c =>
+    {
+      c.SwaggerDocumentName = "v1";
+      c.Language = GenerationLanguage.TypeScript;
+      c.OutputPath = outputPath;
+      c.ClientNamespaceName = "MyCompanyName";
+      c.ClientClassName = "MyTsClient";
+
+    });
 app.Run();
 
 
---
+++ libs/swagger/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/api/index';
+export * from './lib/myTsClient';
+export * from './lib/models/index';
